fb2ff06cc176db89d5ee5eabf7a3327a097a8d40
Freddy <fredericdouamba2014@gmail.com>
2023-11-13 03:50:48 -0800

    Initial Commit

---
+++ AddingSearch/AddingSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingSearchApp.swift
+//  AddingSearch
+//
+//  Created by Student Account on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AddingSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
